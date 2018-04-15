f0bd5eea1de36c93ac917ba1daa3adf8beee55a3
MehmetAyan <mehmetayan84@gmail.com>
2018-04-15 23:13:27 +0300

    Add connect disconnet event handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const socketIO = require('socket.io');
 
 const path = require('path');
+const http = require('http');
 
 var app = express();
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`Server started on PORT ${port}`));
+io.on('connection', (socket) => {
+   console.log('New user is connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected from the server');
+    });
+});
+
+
+
+server.listen(port, () => console.log(`Server started on PORT ${port}`));
