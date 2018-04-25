0bbe027aebd90c05c5d9a5c3319bd0670c0fe57f
MehmetAyan <mehmetayan84@gmail.com>
2018-04-25 21:58:52 +0300

    "Project is completed"

--- server/server.js
+++ server/server.js
@@ -38,7 +38,10 @@ io.on('connection', (socket) => {
    });
 
    socket.on('createMessage', (message, callback) => {
-       io.emit('newMessage', generateMessage(message.from, message.text));
+       var user = users.getUser(socket.id);
+       if(user && isRealString(message.text)) {
+           io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+       }
        callback();
        // socket.broadcast.emit('newMessage', {
        //     from: message.from,
@@ -48,7 +51,10 @@ io.on('connection', (socket) => {
    });
 
    socket.on('createLocationMessage', (message, callback) => {
-      io.emit('newLocationMessage',generateLocationMessage('User', message.latitude, message.longitude));
+      var user = users.getUser(socket.id);
+      if(user) {
+        io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, message.latitude, message.longitude));
+      }
       callback();
    });
 
